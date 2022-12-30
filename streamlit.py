9cf49cd64d3399ed5ebbc247f0a1952c1c3d296c
a-collin <alexis.collin@outlook.fr>
2022-12-30 23:31:08 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,24 +1,25 @@
 import torch
 import streamlit as st
-#import pytorch-lightning
-#import transformers
-#import datasets
-#import wandb
+import pytorch_lightning
+import transformers
+import datasets
+import wandb
 
 #model = torch.load(PATH)
 #model.eval()
 
-def predict(entree_fonction):
-	liste_entree = entree_fonction.split(";")
-	#prediction = model.predict(liste_entree)
-	#return prediction
+def predict(name, skills, exp, job):
+        prediction = model.predict(name, skills, exp, job)
+        return prediction
 
 st.title('Génération de lettre de motivation')
 st.image("""https://rodriguedemeuse.be/wp-content/uploads/2022/02/image-from-rawpixel-id-3285747-jpeg-1024x684-1.jpg""")
 st.header('Entrez les informations requises')
-entree = st.text_input('Paramètre')
+name = st.text_input('Name : ex Nicholas McGregor')
+skills = st.text_input('Skills: ex excellent customer service skills and am familiar with SQL')
+exp = st.text_input('Experiences : ex Wrote numerous unit and API tests')
+job = st.text_input('Job: ex Full Stack Developper')
 
 if st.button('Génération'):
-	#lettre = predict(entree)
-	st.success(lettre)
-	#st.write('hello')
+        lettre = predict(name, skills, exp, job)
+        st.success(lettre)                                
